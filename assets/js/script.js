e6dd1f2de1ed56c0ff3cb6858e784d8c7f3b5fe0
Mike <mikej.miller440@gmail.com>
2021-12-28 15:18:44 -0600

    added event listener for search button and pseudocoded current weather data section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
+// Open weather map api
+var apiKey = ff6fc12478a0481bc7a2df1ec4864f2c
+
+// container for search text
 var citySearchText = "";
 
+// event listener for search button
 $("#search-button").on("click", function() {
     var text = $("#city-search").val();
     if (!text) {
@@ -8,5 +13,14 @@ $("#search-button").on("click", function() {
     else {
         console.log(text);
         citySearchText = text
+
     }
 });
+
+function currentWeather() {
+// Name of City (date: 00/00/0000) icon
+// temp
+// wind speed
+// humidity
+// UV index (with color)
+}
